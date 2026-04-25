b69b635c6238d28aad0a186208d48c0ae77c7709
UmaanMehraj <umaanmehraj5@gmail.com>
2026-04-25 16:12:46 +0530

    add: styles/styles.css

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import '../styles/styles.css'
 
 function GeneralInfo() {
   const [person, setDetails] = useState({
@@ -23,25 +24,35 @@ function GeneralInfo() {
 
   if (isSubmitted) {
     return (
-      <div className='container'>
-        <div className='general'>
-          <h3>Name: {person.name}</h3>
-          <h3>Email: {person.email}</h3>
-          <h3>Phone: {person.phone}</h3>
+      <>
+        <header>
+          <h1>Personal Details</h1>
+        </header>
+        <div className='container'>
+          <div className='general'>
+            <h3>Name: {person.name}</h3>
+            <h3>Email: {person.email}</h3>
+            <h3>Phone: {person.phone}</h3>
+          </div>
+          <button>Edit</button>
         </div>
-        <button>Edit</button>
-      </div>
+      </>
     )
   }
   return (
     <div className='form'>
       <form>
-        <label htmlFor='name'>Name: </label>
-        <input type='text' id='name' name='name' />
-        <label htmlFor='email'>Email:</label>
-        <input type='email' id='email' name='email' />
-        <label htmlFor='phone'>Phone: </label>
-        <input type='tel' id='phone' name='phone' />
+        <label htmlFor='name'>
+          Name:
+          <input type='text' id='name' name='name' />{' '}
+        </label>
+        <label htmlFor='email'>
+          Email:
+          <input type='email' id='email' name='email' />
+        </label>
+        <label htmlFor='phone'>
+          Phone: <input type='tel' id='phone' name='phone' />{' '}
+        </label>
       </form>
       <button type='submit' onClick={updateDetails}>
         Submit
@@ -74,14 +85,19 @@ function EducationalXp() {
   }
   if (isSubmitted) {
     return (
-      <div className='container'>
-        <div className='education'>
-          <h3>School: {education.schoolName}</h3>
-          <h3>Title of Study: {education.title}</h3>
-          <h3>Date of Passing: {education.date}</h3>
+      <>
+        <header>
+          <h1>Educational Details</h1>
+        </header>
+        <div className='container'>
+          <div className='education'>
+            <h3>School: {education.schoolName}</h3>
+            <h3>Title of Study: {education.title}</h3>
+            <h3>Date of Passing: {education.date}</h3>
+          </div>
+          <button>Eidt</button>
         </div>
-        <button>Eidt</button>
-      </div>
+      </>
     )
   }
 
@@ -89,12 +105,16 @@ function EducationalXp() {
     <section>
       <div className='form'>
         <form>
-          <label htmlFor='schoolName'>School: </label>
-          <input type='text' name='schoolName' id='school' />
-          <label htmlFor='title'>Title of Study: </label>
-          <input type='text' name='title' id='title' />
-          <label htmlFor='date'>Date: </label>
-          <input type='date' name='date' id='date' />
+          <label htmlFor='schoolName'>
+            School: <input type='text' name='schoolName' id='school' />
+          </label>
+          <label htmlFor='title'>
+            Title of Study:
+            <input type='text' name='title' id='title' />{' '}
+          </label>
+          <label htmlFor='date'>
+            Date: <input type='date' name='date' id='date' />
+          </label>
         </form>
         <button typeof='submit' onClick={updateEducation}>
           Submit
@@ -113,46 +133,62 @@ function PracticalExp() {
   })
   const [isSubmitted, changeSubmission] = useState(false)
 
-  function updatePractice(){
+  function updatePractice() {
     const company = document.querySelector('#companyName')
     const positonName = document.querySelector('#position')
     const dateStart = document.querySelector('#dateFrom')
     const dateEnd = document.querySelector('#dateEnd')
 
     setPractice({
-      companyName: company.value, position: positonName.value, dateFrom: dateStart.value, dateEnd: dateEnd.value
+      companyName: company.value,
+      position: positonName.value,
+      dateFrom: dateStart.value,
+      dateEnd: dateEnd.value,
     })
 
     changeSubmission(true)
   }
 
-  if(isSubmitted){
-    return(
-      <div className="container">
-        <div className="practice">
-          <h3>Company Name: {practice.companyName}</h3>
-          <h3>Position: {practice.position}</h3>
-          <h3>Date Start: {practice.dateFrom}</h3>
-          <h3>Date End: {practice.dateEnd}</h3>
+  if (isSubmitted) {
+    return (
+      <>
+        <header>
+          <h1>Experience</h1>
+        </header>
+        <div className='container'>
+          <div className='practice'>
+            <h3>Company Name: {practice.companyName}</h3>
+            <h3>Position: {practice.position}</h3>
+            <h3>Date Start: {practice.dateFrom}</h3>
+            <h3>Date End: {practice.dateEnd}</h3>
+          </div>
+          <button>Edit</button>
         </div>
-        <button>Edit</button>
-      </div>
+      </>
     )
   }
 
   return (
     <div className='form'>
       <form>
-        <label htmlFor='comapnyName'>Company Name: </label>
-        <input type='text' id='companyName' name='companyName' />
-        <label htmlFor='position'>Position Ttitle: </label>
-        <input type='text' id='position' name='position' />
-        <label htmlFor='dateFrom'>Date Joined: </label>
-        <input type='date' name='dateFrom' id='dateFrom' />
-        <label htmlFor='dateEnd'>Date End: </label>
-        <input type='date' name='dateEnd' id='dateEnd' />
+        <label htmlFor='comapnyName'>
+          Company Name:{' '}
+          <input type='text' id='companyName' name='companyName' />
+        </label>
+        <label htmlFor='position'>
+          Position Ttitle: <input type='text' id='position' name='position' />
+        </label>
+        <label htmlFor='dateFrom'>
+          Date Joined: <input type='date' name='dateFrom' id='dateFrom' />
+        </label>
+
+        <label htmlFor='dateEnd'>
+          Date End: <input type='date' name='dateEnd' id='dateEnd' />
+        </label>
       </form>
-      <button type="submit" onClick={updatePractice}>Submit</button>
+      <button type='submit' onClick={updatePractice}>
+        Submit
+      </button>
     </div>
   )
 }
