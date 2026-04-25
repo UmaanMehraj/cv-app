e2509e393edc7d78a3b754e33cf55febe91e3090
UmaanMehraj <umaanmehraj5@gmail.com>
2026-04-25 08:13:24 +0530

    update: Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,50 @@
-import { useState } from "react";
+import { useState } from 'react'
 
- export default function GeneralInfo(){
-    const [name, setName] = useState('')
+function GeneralInfo() {
+  const [name, setName] = useState('John Doe')
 
-    function updateName (){
-        const input = document.querySelector('#name')
-        setName(input.value)
-    }
+  function updateName() {
+    const input = document.querySelector('#name')
+    setName(input.value)
+  }
 
-    return <section>
-        <div className="form">
-            <form>
-                <label htmlFor="name"></label>
-                <input type="text" id="name" name="name" />
-            </form>
-        </div>
+  return (
+    <section>
+      <div className='form'>
+        <form>
+          <label htmlFor='name'>Name: </label>
+          <input type='text' id='name' name='name' />
+        </form>
+      </div>
     </section>
+  )
+}
+
+function EducationalXp() {
+  const [schoolName, setSchoolName] = useState('KT middle school')
+
+  function updatetSchoolName() {
+    const input = document.querySelector('#school')
+    setSchoolName(input.value)
+  }
+
+  return (
+    <section>
+      <div className='form'>
+        <form>
+          <label htmlFor='schoolName'>School: </label>
+          <input type='text' name='schoolName' id='school' />
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default function Form() {
+  return (
+    <>
+      <GeneralInfo />
+      <EducationalXp/>
+    </>
+  )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import GeneralInfo from './components/Form.jsx'
+import Form from './components/Form.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GeneralInfo/>
+    <Form/>
   </StrictMode>,
 )
